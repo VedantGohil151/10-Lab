c003937dddac935e50a50375396fb8160c37f5fd
VedantGohil151 <vedantsinhgohil28@gmail.com>
2025-04-24 19:49:07 +0530

    Q3.py

--- Q3.py
+++ Q3.py
@@ -1,7 +1,7 @@
 name=input("Enter your name: ")
 number=input("Enter your mobile number: ")
 email=input("Enter you email id: ")
-f=open('D:\\Krish\\Python\\Details.vcf','a+')
+f=open('D:\\Vedant\\Python\\Details.vcf','a+')
 data=["BEGIN:VCARD\n","VERSION:4.0\n",f"Name: {name}\n",f"MOB: {number}\n",f"EMAIL: {email}\n","END:VCARD\n"]
 
 for lines in data:
